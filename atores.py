b8a6f9785472f154489beab31b9190a14f123808
Bm@ske08 <bianca.maske@gmail.com>
2020-05-11 12:59:48 -0300

    implementado metodos de atores

--- atores.py
+++ atores.py
@@ -109,7 +109,8 @@ class Passaro(Ator):
         o status dos Passaro deve ser alterado para destruido, bem como o seu caracter
 
         """
-        pass
+        if self.y <= 0:
+            self.status = DESTRUIDO
 
     def calcular_posicao(self, tempo):
         """
@@ -125,17 +126,28 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        if self.foi_lancado():
+        if self._esta_voando():
+        # if self.foi_lancado() and self.status == ATIVO:
             delta_t = tempo - self._tempo_de_lancamento
             self._calcular_posicao_vertical(delta_t)
+            self._calcular_posicao_horizontal(delta_t)
         return super().calcular_posicao(tempo)
     
+    def _esta_voando(self):
+        return self.foi_lancado() and self.status == ATIVO
+
     def _calcular_posicao_vertical(self, delta_t):
         y_atual = self._y_inicial
-        angulo_radianos = math.radians(self._angulo_de_lancamento)
-        y_atual +=  self.velocidade_escalar * delta_t * math.sin(angulo_radianos)
+        # angulo_radianos = math.radians(self._angulo_de_lancamento)
+        y_atual +=  self.velocidade_escalar * delta_t * math.sin(self._angulo_de_lancamento)
         y_atual -= (GRAVIDADE * delta_t**2) / 2
         self.y = y_atual
+    
+    def _calcular_posicao_horizontal(self, delta_t):
+        x_atual = self._x_inicial
+        # angulo_radianos = math.radians(self._angulo_de_lancamento)
+        x_atual += self.velocidade_escalar * delta_t * math.cos(self._angulo_de_lancamento)
+        self.x = x_atual
 
 
 
@@ -148,14 +160,14 @@ class Passaro(Ator):
         :param tempo_de_lancamento:
         :return:
         """
-        self._angulo_de_lancamento = angulo
+        self._angulo_de_lancamento = math.radians(angulo)
         self._tempo_de_lancamento = tempo_de_lancamento
 
 
 class PassaroAmarelo(Passaro):
     _caracter_ativo = 'A'
     _caracter_destruido = 'a'
-    velociade_escalar = 30
+    velocidade_escalar = 30
 
 
 class PassaroVermelho(Passaro):
